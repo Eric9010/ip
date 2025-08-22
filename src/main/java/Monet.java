a52225934be6affabcc1ff87ff674abf437c7bd9
EricWei <194746012+Eric9010@users.noreply.github.com>
2025-08-22 12:03:34 +0800

    Level 3: Mark as Done + Task Class

--- src/main/java/Monet.java
+++ src/main/java/Monet.java
@@ -4,25 +4,41 @@ public class Monet {
     public static void main(String[] args) {
         String chatbotName = "Monet";
         String divider = "____________________________________________________________________________________________";
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         System.out.println(" Hello! I'm " + chatbotName);
         System.out.println(" What can I do for you?");
         System.out.println(divider);
-        String[] tasks = new String[100]; // Array to store up to 100 tasks
-        int taskCount = 0; // To keep track of the number of tasks
 
         Scanner in = new Scanner(System.in);
 
         while (true) {
             String input = in.nextLine();
+            String[] parts = input.split(" ", 2); // Split command from arguments
+            String command = parts[0];
+
+
             if (input.equals("bye")) {
                 break; // Exit the loop if user types "bye"
             }else if (input.equals("list")){
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println("  " + (i + 1) + ". " + tasks[i]); // List out stored tasks
+                    // List out stored tasks
+                    System.out.println("  " + (i + 1) + ". " + tasks[i]);
                 }
-            }else{
-                tasks[taskCount] = input; // Add inputted task to list
+            }else if (command.equals("mark")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskIndex]);
+            } else if (command.equals("unmark")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                tasks[taskIndex].unmarkAsDone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskIndex]);
+            } else {
+                // Add a new task
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("  added: " + input);
             }
