5293b0505aada17af587308822858aa40286b86e
EricWei <194746012+Eric9010@users.noreply.github.com>
2025-08-29 12:33:19 +0800

    Add TaskList class

--- src/main/java/Monet.java
+++ src/main/java/Monet.java
@@ -1,21 +1,16 @@
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Monet {
-    /** Static fields for file handing */
-    private static final String FILE_PATH = "./data/monet.txt";
+    private static TaskList tasks;
     private static Storage storage;
-    private static ArrayList<Task> tasks;
 
     public static void main(String[] args) {
-
         String chatbotName = "Monet";
         String divider = "____________________________________________________________";
 
-        /** Now uses the Storage class */
         storage = new Storage("./data/monet.txt");
-        tasks = storage.load();
+        tasks = new TaskList(storage.load());
 
         System.out.println(" Hello! I'm " + chatbotName);
         System.out.println(" What can I do for you?");
@@ -35,15 +30,15 @@ public class Monet {
                 switch (command) {
                 case LIST:
                     System.out.println(" Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println("  " + (i + 1) + "." + tasks.get(i));
+                    for (int i = 0; i < tasks.getSize(); i++) {
+                        System.out.println("  " + (i + 1) + "." + tasks.getTask(i));
                     }
                     break;
 
                 case MARK:
                 case UNMARK: {
-                    int taskIndex = Parser.parseIndex(input, tasks.size());
-                    Task task = tasks.get(taskIndex);
+                    int taskIndex = Parser.parseIndex(input, tasks.getSize());
+                    Task task = tasks.getTask(taskIndex);
                     if (command == Command.MARK) {
                         task.markAsDone();
                         System.out.println(" Nice! I've marked this task as done:");
@@ -52,50 +47,50 @@ public class Monet {
                         System.out.println(" OK, I've marked this task as not done yet:");
                     }
                     System.out.println("   " + task);
-                    storage.save(tasks); // Save changes to the file
+                    storage.save(tasks.getTasks()); // Save changes to the file
                     break;
                 }
 
                 case DELETE: {
-                    int taskIndex = Parser.parseIndex(input, tasks.size());
-                    Task removedTask = tasks.remove(taskIndex);
+                    int taskIndex = Parser.parseIndex(input, tasks.getSize());
+                    Task removedTask = tasks.deleteTask(taskIndex);
                     System.out.println(" Noted. I've removed this task:");
                     System.out.println("   " + removedTask);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    storage.save(tasks); // Save changes to the file
+                    System.out.println(" Now you have " + tasks.getSize() + " tasks in the list.");
+                    storage.save(tasks.getTasks()); // Save changes to the file
                     break;
                 }
 
                 case TODO: {
                     String description = Parser.parseTodo(input);
                     Todo newTodo = new Todo(description);
-                    tasks.add(newTodo);
+                    tasks.addTask(newTodo);
                     System.out.println(" Got it. I've added this task:");
                     System.out.println("   " + newTodo);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    storage.save(tasks); // Save changes to the file
+                    System.out.println(" Now you have " + tasks.getSize() + " tasks in the list.");
+                    storage.save(tasks.getTasks()); // Save changes to the file
                     break;
                 }
 
                 case DEADLINE: {
                     String[] deadlineDetails = Parser.parseDeadline(input);
                     Deadline newDeadline = new Deadline(deadlineDetails[0], deadlineDetails[1]);
-                    tasks.add(newDeadline);
+                    tasks.addTask(newDeadline);
                     System.out.println(" Got it. I've added this task:");
                     System.out.println("   " + newDeadline);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    storage.save(tasks); // Save changes to the file
+                    System.out.println(" Now you have " + tasks.getSize() + " tasks in the list.");
+                    storage.save(tasks.getTasks()); // Save changes to the file
                     break;
                 }
 
                 case EVENT: {
                     String[] eventDetails = Parser.parseEvent(input);
                     Event newEvent = new Event(eventDetails[0], eventDetails[1], eventDetails[2]);
-                    tasks.add(newEvent);
+                    tasks.addTask(newEvent);
                     System.out.println(" Got it. I've added this task:");
                     System.out.println("   " + newEvent);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    storage.save(tasks); // Save changes to the file
+                    System.out.println(" Now you have " + tasks.getSize() + " tasks in the list.");
+                    storage.save(tasks.getTasks()); // Save changes to the file
                     break;
                 }
 
